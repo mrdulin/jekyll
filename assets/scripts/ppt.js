e4b14b3aa259a6b1ef9d6c80b11161ebc28b29f1
novaline <novaline@qq.com>
2016-11-21 15:03:32 +0800

    :add ppt

---
+++ assets/scripts/ppt.js
@@ -0,0 +1,10 @@
+$(function () {
+    Reveal.initialize({
+        width: "100%",
+        height: "100%",
+        margin: 0,
+        minScale: 1,
+        maxScale: 1,
+        slideNumber: true
+    });
+})
